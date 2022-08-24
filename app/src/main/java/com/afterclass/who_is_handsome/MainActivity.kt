a39f95078c014d81e181210fefa9e83d76965ea0
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-08-24 19:13:44 +0530

    Version 1 completed!

---
+++ app/src/main/java/com/afterclass/who_is_handsome/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.afterclass.who_is_handsome
+
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.annotation.RequiresApi
+
+
+class MainActivity : AppCompatActivity() {
+    companion object{
+        val TAG = "Main Activity"
+        val JOB_ID = 123
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun scheduleJob(view: View){
+
+        val componentName=ComponentName(this,ToastJobService::class.java)
+
+        val jobInfo = JobInfo.Builder(JOB_ID,componentName)
+            .setPersisted(true)
+            .setPeriodic(15*60*1000)
+            .build()
+
+        val scheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+        val resultCode = scheduler.schedule(jobInfo)
+        if(resultCode == JobScheduler.RESULT_SUCCESS){
+            Log.d(TAG,"Job Scheduled!")
+        }
+        else{
+            Log.d(TAG,"Job Scheduling failed!")
+        }
+    }
+
+    fun cancelJob(view:View){
+        val scheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+        scheduler.cancel(JOB_ID)
+        Log.d(TAG,"Job Cancelled!")
+    }
+}
---
+++ app/src/main/java/com/afterclass/who_is_handsome/ToastJobService.kt
@@ -0,0 +1,36 @@
+package com.afterclass.who_is_handsome
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Intent
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+
+class ToastJobService:JobService() {
+    companion object{
+        val TAG="ToastJobService"
+        var jobCancelled = false
+
+    }
+    override fun onStartJob(params: JobParameters?): Boolean {
+        Log.d(TAG,"Job Started!")
+        doBackgroundWork(params)
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        Log.d(TAG,"Job cancelled before completion!")
+        jobCancelled=true
+        return true
+    }
+    fun doBackgroundWork(params: JobParameters?){
+        Thread(Runnable {
+            Looper.prepare()
+            Toast.makeText(this,"You are handsome!",Toast.LENGTH_LONG).show()
+            Log.d(TAG,"Job Finished!")
+            jobFinished(params,false)
+
+        }).start()
+    }
+}
